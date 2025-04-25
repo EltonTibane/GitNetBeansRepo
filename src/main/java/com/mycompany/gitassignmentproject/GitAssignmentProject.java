f1a76e948bce9a9cdad419cb45951b39f14c15a0
EltonTibane <eltonexclusive20@gmail.com>
2025-04-25 21:28:43 +0200

    Initial commit with Hello World

---
+++ src/main/java/com/mycompany/gitassignmentproject/GitAssignmentProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitassignmentproject;
+
+/**
+ *
+ * @author elton
+ */
+public class GitAssignmentProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello NetBeans. Welcome to version control using Git.");
+    }
+}
